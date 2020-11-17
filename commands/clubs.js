c56bca512cb91882ba20ddf6d789e8955f59e145
edwardshturman <60242511+edwardshturman@users.noreply.github.com>
2020-11-16 20:35:03 -0800

    Update clubs.js

--- commands/clubs.js
+++ commands/clubs.js
@@ -1,9 +1,22 @@
 module.exports = {
     name: 'clubs',
-    description: "Fetches d.games clubs.",
+    description: "Sends list of d.games-supported clubs.",
     execute(message, args) {
         const Discord = require('discord.js');
-        const clubsEmbed = new Discord.MessageEmbed()
+        message.channel.send("<:dlineleft:778112794120945724><:dlinecenter:778112794117406740><:dlineright:778112793882525717> <:dscord:778113952860209173> ***CLUBS*** <:dscord:778113952860209173> <:dlineleft:778112794120945724><:dlinecenter:778112794117406740><:dlineright:778112793882525717>");
+        const riotEmbed = new Discord.MessageEmbed()
+            .setColor('#ff3300')
+            .setTitle('Riot Club')
+            .setDescription('Into League of Legends and/or Valorant? We\'ve got ya covered. Map play design thinking—whatever that means—comp strategy, and of course, HSEL (High School Esports League). Join the fun and sign up for Winter tryouts today!')
+            .setThumbnail('https://dcraft.net/wp-content/uploads/2020/09/riot-club-background-from-valorant-site-circle.png');
+        message.channel.send(riotEmbed);
+        const gAndAEmbed = new Discord.MessageEmbed()
+            .setColor('#ff3300')
+            .setTitle('Game and Animation Club')
+            .setDescription('Unity, Unreal, GameMaker Studio, we\'ve got it all. Whether you\'re a beginner looking to learn the ropes of game design, or a pro looking to compete with senio—I mean, help out the team, you\'ll do great. Stay tuned for signup info!')
+            .setThumbnail('https://dcraft.net/wp-content/uploads/2020/11/GandAlogo-circular.png');
+        message.channel.send(gAndAEmbed);
+        /*const clubsEmbed = new Discord.MessageEmbed()
             .setColor('#ff3300')
             .setTitle('d.games clubs')
             .setURL('https://community.dcraft.net/clubs')
@@ -11,6 +24,6 @@ module.exports = {
             .addField('Game & Animation Club', ':notes: `Control music in Discord`\n:speech_balloon: `Color and format in chat`\n:art: `Sign color and formatting`\n:sparkles: `Special prefix`\n<:elytra:742960266702094436> `Elytra at spawn`\n:crescent_moon: `See the building in its beautiful nighttime mode!`\n:moneybag: `Low on d.coins? No problem! Now you\'re able to take out a loan.`\n<:enderchest:742961065628794911> `Enjoy your own portable Enderchest in non-survival gamemodes`\n<:firework:742961394110038106> `Access fireworks via commands!`\n:star2: `Priority server joining`\n:hash: `Access to early updates channel and ability to comment on updates before they launch`\n<:canvascbl:742961937020878930> `See grades via CanvasCBL`', true)
             .addField('Riot Club', '**Everything in Dragon, plus:**\n:pen_ballpoint: `Nickname editing`\n<:crafting_table:742964601943228506> `Show crafting recipe by command`\n<:grass_block:742965330418204746> `You know those 20 x 20 plots? Say goodbye—we\'re giving you your own world!`', true)
             .setFooter('For more information, click the d.games clubs title above!', 'https://dcraft.net/wp-content/uploads/2020/03/server-icon-dark-outline-1.png');
-        message.channel.send(clubsEmbed);
+        message.channel.send(clubsEmbed);*/
     }
 };
