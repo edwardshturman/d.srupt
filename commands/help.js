769fc63d643fd6493cb5aa023f758b61faad48b9
edwardshturman <60242511+edwardshturman@users.noreply.github.com>
2020-12-14 13:02:52 -0800

    Comment out music commands in help

--- commands/help.js
+++ commands/help.js
@@ -11,7 +11,7 @@ module.exports = {
             .addField('d.ping', 'does what you think it does', false)
             .addField('d.yt', 'sends a link to the glorious d.craft YouTube', false)
             .addField('d.verify', 'super high level tactic, remember it, yeah?', false) // testing, use role check from command and remove in production
-            .addField('d.play // d.skip // d.stop // d.np // d.q // d.pause // d.resume', 'low-level "music commands"', false)
+            // .addField('d.play // d.skip // d.stop // d.np // d.q // d.pause // d.resume', 'low-level "music commands"', false)
             .addField('d.ranks', 'displays d.craft ranks', false) // maybe mod-only
             .addField('d.clubs', 'displays list of d.tech gaming clubs d.games supports', false) // mod-only, remove in production
             .addField('d.list', 'displays list of d.games servers', false) // mod-only, remove in production
