80aaf2401db83f72f94203d31cd2e9c36f3b75ce
edwardshturman <60242511+edwardshturman@users.noreply.github.com>
2021-04-15 20:28:18 -0700

    Rewrite args check in suggest command

--- commands/suggest.js
+++ commands/suggest.js
@@ -5,10 +5,10 @@ module.exports = {
         const Discord = require('discord.js');
         const channel = message.guild.channels.cache.get('610552731555594240');
         if (!channel) return message.channel.send('Suggestions channel does not exist!');
-        if (!args) return message.delete();
+        if (!args[0]) return message.delete();
 
         let messageArgs = args.join(' ');
-        if (args[0] === 'b') {
+        if (args[0]) {
             const embed = new Discord.MessageEmbed()
                 .setColor('#ff3300')
                 .setAuthor(message.member.displayName, message.author.displayAvatarURL({dynamic: true}))
